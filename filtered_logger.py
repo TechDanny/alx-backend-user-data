71eeaad7f5b2814520a8e1ac46837fae3d78bd7f
root <kokidanny15@gmail.com>
2024-02-09 02:13:00 +0300

    regex-ing

---
+++ filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Regex-ing
+"""
+
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    returns the log message obfuscated
+    """
+    log_message = re.sub(fr'({re.escape(separator)}{"|".join(map(re.escape, fields))}=)[^;]+', fr'\1{redaction}', message)
+    return log_message
