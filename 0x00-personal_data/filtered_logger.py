605ef76e8df6364a6130a5a9302310a944f542df
root <kokidanny15@gmail.com>
2024-02-09 15:28:36 +0300

    Read and filter data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -62,10 +62,35 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     """
     returns a connector to the database
     """
-    connector = mysql.connector.connection.MYSQLConnection(
+    connector = mysql.connector.connect(
         host=os.getenv('PERSONAL_DATA_DB_HOST', "localhost"),
         database=os.getenv('PERSONAL_DATA_DB_NAME'),
         user=os.getenv('PERSONAL_DATA_DB_USERNAME', "root"),
         password=os.getenv('PERSONAL_DATA_DB_PASSWORD', "")
     )
     return connector
+
+
+def main():
+    """
+    it obtains a database connection using get_db and retrieve all
+    rows in the users table and display each row under a filtered format like this:
+    """
+    connection = get_db()
+    logger = get_logger()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    for x in rows:
+        message = (
+            "name={}; email={}; phone={}; ssn={}; "
+            "password={}; ip={}; last_login={}; user_agent={};"
+        ).format(
+            x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7])
+        logger.info(message)
+    cursor.close()
+    connection.close()
+
+
+if __name__ == "__main__":
+    main()
