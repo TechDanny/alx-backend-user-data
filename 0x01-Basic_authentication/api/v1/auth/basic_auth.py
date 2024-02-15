8ca36d94ec85420424f031e36531c4e745585a54
root <kokidanny15@gmail.com>
2024-02-15 20:17:49 +0300

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,10 +5,22 @@ Basic auth
 
 
 from .auth import Auth
+import re
 
 
 class BasicAuth(Auth):
     """
     inherits from Auth
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+         returns the Base64 part of the Authorization header for a
+         Basic Authentication
+        """
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            fieldMatch = re.fullmatch(pattern, authorization_header.strip())
+            if fieldMatch is not None:
+                return fieldMatch.group('token')
+        return None
