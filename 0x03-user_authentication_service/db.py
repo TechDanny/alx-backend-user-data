dcf41241cf0a6e7a4b87accf6dc8f19ac360e6a7
root <kokidanny15@gmail.com>
2024-02-23 00:58:58 +0300

    changes made

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -48,11 +48,12 @@ class DB:
         returns the first row found in the users
         table as filtered by the method’s input arguments.
         """
-        for key in kwargs.keys():
-            if not hasattr(User, key):
-                raise InvalidRequestError
-
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if user is None:
-            raise NoResultFound
-        return user
+        session = self._session
+        try:
+            query = session.query(User).filter_by(**kwargs)
+            reslt = query.one()
+        except NoResultFound:
+            raise NoResultFound()
+        except InvalidRequestError:
+            raise InvalidRequestError()
+        return reslt
